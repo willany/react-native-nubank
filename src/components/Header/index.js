35a9334735b8c831a497fef43c3a603f867e5d42
wsrmelo <willany_lp@hotmail.com>
2019-05-03 17:59:10 -0300

    First Commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import logo from '../../assets/Nubank_Logo.png';
+
+import {
+  Container, Top, Logo, Title,
+} from './styles';
+
+
+export default function Header() {
+  return (
+    <Container>
+      <Top>
+        <Logo src={logo} />
+        <Title>Willany Silva</Title>
+      </Top>
+      <Icon name="keyboard-arrow-down" size={20} color="#FFF" />
+    </Container>
+  );
+}
