99ae544d41c00d3697696f69160e3aed773869b0
willanysilva <willany_lp@hotmail.com>
2019-05-05 00:30:33 -0300

    add animation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Icon from "react-native-vector-icons/MaterialIcons";
 
+import { Animated } from "react-native";
+import { PanGestureHandler, State } from "react-native-gesture-handler";
+
 import Header from "../../components/Header";
 import Tabs from "../../components/Tabs";
+import Menu from "../../components/Menu";
 import {
   Container,
   Content,
@@ -16,29 +20,85 @@ import {
 } from "./styles";
 
 export default function Main() {
+  let offset = 0;
+  const translateY = new Animated.Value(0);
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY
+        }
+      }
+    ],
+    { useNativeDriver: true }
+  );
+  function onHandlerStateChanged(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const { translationY } = event.nativeEvent;
+      offset += translationY;
+
+      if (translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true
+      }).start(() => {
+        offset = opened ? 380 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+    }
+  }
   return (
     <Container>
       <Header />
       <Content>
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="visibility-off" size={28} color="#666" />
-          </CardHeader>
+        <Menu translateY={translateY} />
 
-          <CardContent>
-            <Title>Saldo disponível</Title>
-            <Description>R$ 197,98</Description>
-          </CardContent>
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChanged}
+        >
+          <Card
+            style={{
+              transform: [
+                {
+                  translateY: translateY.interpolate({
+                    inputRange: [-350, 0, 380],
+                    outputRange: [-50, 0, 380],
+                    extrapolate: "clamp"
+                  })
+                }
+              ]
+            }}
+          >
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
 
-          <CardFooter>
-            <Annotation>
-              Tranfesrência de R$ 20,00 recebida de Willamis da Silva às 06:00h
-            </Annotation>
-          </CardFooter>
-        </Card>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$ 197,98</Description>
+            </CardContent>
+
+            <CardFooter>
+              <Annotation>
+                Tranfesrência de R$ 20,00 recebida de Willamis da Silva às
+                06:00h
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
       </Content>
-      <Tabs />
+      <Tabs translateY={translateY} />
     </Container>
   );
 }
